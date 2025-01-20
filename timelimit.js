baf4e7cf26caef30c18fd925706c6bfa08f65e86
Saurabh Prakash <saurabhp75@gmail.com>
2025-01-21 01:37:42 +0530

    Add more problems

--- timelimit.js
+++ timelimit.js
@@ -1,13 +1,33 @@
 // ToDo
 var timeLimit = function (fn, t) {
-  return async function (...args) {};
+  return async function (...args) {
+    // wait for t seconds before rejecting with time limit exceeded
+    return new Promise(async (resolve, reject) => {
+      const id = setTimeout(() => reject("Time Limit Exceeded"), t);
+      try {
+        const res = await fn(...args);
+        resolve(res);
+      } catch (error) {
+        reject(error);
+      } finally {
+        clearTimeout(id);
+      }
+      //   fn(...args)
+      //     .then((res) => resolve(res))
+      //     .catch((error) => console.log(error))
+      //     .finally(() => clearTimeout(id));
+    });
+  };
 };
 
 // Sample input function
 function fn(t) {
-  return new Promise((res) => setTimeout(res, t));
+  return new Promise((res) => setTimeout(() => res("saurabhp75"), t));
 }
 
 const limited = timeLimit(fn, 100);
 
-limited(150).catch(console.log); // “Time Limit Exceeded” at t=100ms
+limited(150)
+  .then((res) => console.log(res))
+  .catch(console.log) // “Time Limit Exceeded” at t=100ms
+  .finally(() => console.log("finally called"));
---
+++ timeout-cancel.js
@@ -0,0 +1,13 @@
+var cancellable = function (fn, args, t) {
+  const timeoutId = setTimeout(function () {
+    fn.apply(null, args);
+  }, t);
+
+  const cancelFn = function () {
+    clearTimeout(timeoutId);
+  };
+
+  return cancelFn;
+};
+
+setTimeout(cancelFn, cancelTimeMs);
